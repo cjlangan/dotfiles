0a3ad56cdf4f1b2bf446d4e86047b99cf30e97d7
cjlangan <connorjameslangan@gmail.com>
2025-09-19 19:01:39 -0500

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,71 +1,66 @@
 vim.o.relativenumber = true
-vim.o.termguicolors = true
+vim.o.number = true
 vim.o.breakindent = true
+vim.o.termguicolors = true
 vim.o.swapfile = false
 vim.o.showmode = false
 vim.o.expandtab = true
 vim.o.undofile = true
-vim.o.number = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 vim.o.wrap = false
 vim.o.clipboard = "unnamedplus"
 vim.o.winborder = "rounded"
 vim.o.signcolumn = "yes"
 vim.o.mouse = "a"
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
 vim.o.scrolloff = 8
-vim.o.tabstop = 4
+vim.o.sidescrolloff = 8
+vim.o.softtabstop = 2
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.completeopt = "menuone"
+vim.o.pumheight = 7
+vim.o.foldmethod = "indent"
+vim.o.foldenable = false
 vim.g.mapleader = " "
 
 vim.pack.add({
-    {src = "https://github.com/christoomey/vim-tmux-navigator"},
-    {src = "https://github.com/lewis6991/gitsigns.nvim"},
-    {src = "https://github.com/vague2k/vague.nvim"},
-    {src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main"},
-    {src = "https://github.com/neovim/nvim-lspconfig"},
-    {src = "https://github.com/mbbill/undotree"},
-    {src = "https://github.com/echasnovski/mini.pick"},
-    {src = "https://github.com/stevearc/oil.nvim"},
-    {src = "https://github.com/windwp/nvim-autopairs"},
-    {src = "https://github.com/cbochs/grapple.nvim"},
+  {src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main"},
+  {src = "https://github.com/christoomey/vim-tmux-navigator"},
+  {src = "https://github.com/lewis6991/gitsigns.nvim"},
+  {src = "https://github.com/neovim/nvim-lspconfig"},
+  {src = "https://github.com/windwp/nvim-autopairs"},
+  {src = "https://github.com/mfussenegger/nvim-dap"},
+  {src = "https://github.com/mason-org/mason.nvim"},
+  {src = "https://github.com/vague2k/vague.nvim"},
+  {src = "https://github.com/stevearc/oil.nvim"},
+  {src = "https://github.com/ibhagwan/fzf-lua"},
+  {src = "https://github.com/tpope/vim-dadbod"},
+  {src = "https://github.com/mbbill/undotree"},
+  {src = "https://github.com/3rd/image.nvim"},
 })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(ev)
-        local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if client:supports_method('textDocument/completion') then
-            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-        end
-    end,
-})
-vim.cmd("set completeopt+=noselect")
-
-require("grapple").setup({ icons = false })
-require("nvim-autopairs").setup()
-require("mini.pick").setup()
 require("vague").setup({ transparent = true })
+require("nvim-autopairs").setup()
+-- require("mini.pick").setup()
+require("fzf-lua").setup()
+require("mason").setup()
 require("oil").setup()
-require'nvim-treesitter'.install { "markdown", "python", "javascript", "java", "c" }
--- require "nvim-treesitter.configs".setup({
--- 	ensure_installed = { "svelte", "typescript", "javascript" },
--- 	highlight = { enable = true }
--- })
+require("image").setup({ tmux_show_only_in_active_window = true })
+require("nvim-treesitter").install({ "markdown", "python", "javascript",
+"java", "c" , "sql", "typst"})
 
-vim.lsp.enable({ "lua_ls", "jdtls", "ts_ls", "clangd", "pyright" })
+vim.lsp.enable({ "lua_ls", "jdtls", "ts_ls", "clangd", "texlab",
+"pyright" , "postgres_lsp", "tinymist" })
 vim.cmd("colorscheme vague")
 vim.cmd(":hi statusline guibg=NONE")
 
-vim.keymap.set("n", "<leader>f", ":Pick files<cr>")
-vim.keymap.set("n", "<leader>g", ":Pick grep_live<cr>")
-vim.keymap.set("n", "<leader>h", ":Pick help<cr>")
+
+vim.keymap.set("n", "<leader>f", ":FzfLua files<cr>")
+vim.keymap.set("n", "<leader>g", ":FzfLua live_grep_native<cr>")
+vim.keymap.set("n", "<leader>q", ":FzfLua diagnostics_document<cr>")
 vim.keymap.set("n", "<leader>e", ":Oil<cr>")
 vim.keymap.set("n", "<leader>u", ":UndotreeToggle<cr>")
-vim.keymap.set("n", "<A-a>", "<cmd>Grapple toggle<cr>")
-vim.keymap.set("n", "<A-e>", "<cmd>Grapple toggle_tags<cr>")
-vim.keymap.set("n", "<A-j>", "<cmd>Grapple select index=1<cr>")
-vim.keymap.set("n", "<A-k>", "<cmd>Grapple select index=2<cr>")
-vim.keymap.set("n", "<A-l>", "<cmd>Grapple select index=3<cr>")
-vim.keymap.set("n", "<A-;>", "<cmd>Grapple select index=4<cr>")
 
 vim.keymap.set('n', '<ESC>', '<cmd>nohlsearch<CR>')
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -75,5 +70,3 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<S-g>", "<S-g>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
-
